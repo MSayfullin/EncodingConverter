b6bb946367c5a5107b1d0fa0698616a1758f0ceb
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-05-09 00:23:05 +0400

    Support more granular exceptions handling in FileManager

--- EncodingConverter/Logic/FileManager.cs
+++ EncodingConverter/Logic/FileManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security;
+using dokas.EncodingConverter.Exceptions;
 
 namespace dokas.EncodingConverter.Logic
 {
@@ -43,21 +45,213 @@ namespace dokas.EncodingConverter.Logic
         {
             return SettingsSupplier.SearchPatterns
                 .AsParallel()
-                .SelectMany(pattern => Directory.EnumerateFiles(_sourcePath.Value, pattern, SearchOption.AllDirectories));
+                .SelectMany(pattern => GetFilesBy(pattern));
         }
 
         public byte[] Load(string filePath)
         {
-            return File.ReadAllBytes(filePath);
+            return LoadFile(filePath);
         }
 
         public void Save(string filePath, byte[] bytes)
         {
+            EnsureDestinationFolderExists();
+            SaveFile(filePath, bytes);
+        }
+
+        #region Helpers
+
+        private IEnumerable<string> GetFilesBy(string pattern)
+        {
+            try
+            {
+                return Directory.EnumerateFiles(_sourcePath.Value, pattern, SearchOption.AllDirectories);
+            }
+            catch (ArgumentNullException)
+            {
+                // if it fails, then something is very wrong
+                throw;
+            }
+            catch (ArgumentException ex)
+            {
+                throw new RecoverableException(
+                    "Source folder is not specified or has incorrect symbols in path.", ex);
+            }
+            catch (PathTooLongException ex)
+            {
+                throw new RecoverableException(
+                    "Source folder path exceeds the system-defined maximum length. Paths must be less than 248 characters.", ex);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new RecoverableException(
+                    "Source folder path is invalid: " + ex.Message, ex);
+            }
+            catch (IOException ex)
+            {
+                throw new RecoverableException(ex.Message, ex);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                throw new RecoverableException(
+                    "Source folder cannot be read." + Environment.NewLine + ex.Message, ex);
+            }
+            catch (SecurityException ex)
+            {
+                throw new RecoverableException(
+                    "Source folder cannot be read. Check that you have sufficient permissions.", ex);
+            }
+        }
+
+        private void EnsureDestinationFolderExists()
+        {
             if (!Directory.Exists(_destinationPath.Value))
             {
-                Directory.CreateDirectory(_destinationPath.Value);
+                try
+                {
+                    Directory.CreateDirectory(_destinationPath.Value);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    throw new RecoverableException(
+                        "Destination folder is not specified.", ex);
+                }
+                catch (ArgumentException ex)
+                {
+                    throw new RecoverableException(
+                        "Destination folder is not specified or has incorrect symbols in path.", ex);
+                }
+                catch (NotSupportedException ex)
+                {
+                    throw new RecoverableException(
+                        "Destination folder path contains a colon character (:) that is not part of a drive label ('C:\').", ex);
+                }
+                catch (PathTooLongException ex)
+                {
+                    throw new RecoverableException(
+                        "Destination folder path exceeds the system-defined maximum length. Paths must be less than 248 characters.", ex);
+                }
+                catch (DirectoryNotFoundException ex)
+                {
+                    throw new RecoverableException(
+                        "Destination folder path is invalid: " + ex.Message, ex);
+                }
+                catch (IOException ex)
+                {
+                    throw new RecoverableException(ex.Message, ex);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    throw new RecoverableException(
+                        "Destination folder cannot be created. Check that you have sufficient permissions.", ex);
+                }
             }
-            File.WriteAllBytes(Path.Combine(_destinationPath.Value, Path.GetFileName(filePath)), bytes);
         }
+
+        private byte[] LoadFile(string filePath)
+        {
+            try
+            {
+                return File.ReadAllBytes(filePath);
+            }
+            catch (ArgumentNullException)
+            {
+                // this should have been failed earlier
+                // if it fails here something is went wrong
+                throw;
+            }
+            catch (ArgumentException ex)
+            {
+                throw new RecoverableException(
+                    "File path is not specified or has incorrect symbols in path.", ex);
+            }
+            catch (NotSupportedException ex)
+            {
+                throw new RecoverableException(
+                    "File path is not specified or has incorrect symbols in path.", ex);
+            }
+            catch (PathTooLongException ex)
+            {
+                throw new RecoverableException(
+                    "File name exceeds the system-defined maximum length. It must be less than 260 characters.", ex);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new RecoverableException(
+                    "Source folder path is invalid: " + ex.Message, ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new RecoverableException(
+                    "File cannot be read from path specified.", ex);
+            }
+            catch (IOException ex)
+            {
+                throw new RecoverableException(ex.Message, ex);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                throw new RecoverableException(
+                    "File cannot be read." + Environment.NewLine + ex.Message, ex);
+            }
+            catch (SecurityException ex)
+            {
+                throw new RecoverableException(
+                    "File cannot be read. Check that you have sufficient permissions.", ex);
+            }
+        }
+
+        private void SaveFile(string filePath, byte[] bytes)
+        {
+            try
+            {
+                File.WriteAllBytes(Path.Combine(_destinationPath.Value, Path.GetFileName(filePath)), bytes);
+            }
+            catch (ArgumentNullException)
+            {
+                // this should have been failed earlier
+                // if it fails here something is went wrong
+                throw;
+            }
+            catch (ArgumentException)
+            {
+                // this should have been failed earlier
+                // if it fails here something is went wrong
+                throw;
+            }
+            catch (NotSupportedException)
+            {
+                // this should have been failed earlier
+                // if it fails here something is went wrong
+                throw;
+            }
+            catch (PathTooLongException ex)
+            {
+                throw new RecoverableException(
+                    "File name exceeds the system-defined maximum length. It must be less than 260 characters.", ex);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                // this should have been failed earlier
+                // if it fails here something is went wrong
+                throw;
+            }
+            catch (IOException ex)
+            {
+                throw new RecoverableException(ex.Message, ex);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                throw new RecoverableException(
+                    "Converted file cannot be created." + Environment.NewLine + ex.Message, ex);
+            }
+            catch (SecurityException ex)
+            {
+                throw new RecoverableException(
+                    "Converted file cannot be created. Check that you have sufficient permissions.", ex);
+            }
+        }
+
+        #endregion
     }
 }
